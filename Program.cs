a1247e6cb69c3d8f9da913b07926d9bd75e48164
IAMebonyhope <blessyn2hope@gmail.com>
2024-01-20 01:20:32 -0800

    add manual instrumentation setup

--- Program.cs
+++ Program.cs
@@ -1,57 +1,34 @@
-using System.Globalization;
-
-using Microsoft.AspNetCore.Mvc;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
+var serviceName = "dice-server";
+var serviceVersion = "1.0.0";
+
 var builder = WebApplication.CreateBuilder(args);
 
-const string serviceName = "roll-dice";
-
-builder.Logging.AddOpenTelemetry(options =>
-{
-    options
-        .SetResourceBuilder(
-            ResourceBuilder.CreateDefault()
-                .AddService(serviceName))
-        .AddConsoleExporter();
-});
-
 builder.Services.AddOpenTelemetry()
-      .ConfigureResource(resource => resource.AddService(serviceName))
-      .WithTracing(tracing => tracing
-          .AddAspNetCoreInstrumentation()
-          .AddConsoleExporter())
-      .WithMetrics(metrics => metrics
-          .AddAspNetCoreInstrumentation()
-          .AddConsoleExporter());
+    .ConfigureResource(resource => resource.AddService(
+        serviceName: serviceName, 
+        serviceVersion: serviceVersion))
+    .WithTracing(tracing => tracing
+        .AddSource(serviceName)
+        .AddConsoleExporter())
+    .WithMetrics(metrics => metrics
+        .AddMeter(serviceName)
+        .AddConsoleExporter());
+
+builder.Logging.AddOpenTelemetry(options => options
+    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(
+        serviceName: serviceName,
+        serviceVersion: serviceVersion))
+    .AddConsoleExporter());
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-
-string HandleRollDice([FromServices]ILogger<Program> logger, string? player)
-{
-    var result = RollDice();
-
-    if (string.IsNullOrEmpty(player))
-    {
-        logger.LogInformation("Anonymous player is rolling the dice: {result}", result);
-    }
-    else
-    {
-        logger.LogInformation("{player} is rolling the dice: {result}", player, result);
-    }
-
-    return result.ToString(CultureInfo.InvariantCulture);
-}
-
-int RollDice()
-{
-    return Random.Shared.Next(1, 7);
-}
-
-app.MapGet("/rolldice/{player?}", HandleRollDice);
+app.MapControllers();
 
 app.Run();
